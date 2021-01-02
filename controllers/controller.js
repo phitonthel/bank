3ad211bd200b5963faa96494320d5591e9f52804
phitonthel <thephitonthel@gmail.com>
2021-01-02 16:03:01 +0700

    release 5

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,11 +2,19 @@ const {Customer, Account} = require('../models/index')
 
 class Controller {
   static home(req, res) {
-
+    res.render('homepage', {})
   }
 
   static showCustomers(req, res) {
-
+    Customer.findAll({
+      order: [['fullName', 'ASC']]
+    })
+    .then((data) => {
+      res.render('showCustomers', {data})
+    })
+    .catch((err) => {
+      res.send(err)
+    })
   }
 
   static registerCustomer(req, res) {
