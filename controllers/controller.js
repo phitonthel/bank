c60e07ad1714c975ae1c5fe5c2cc5df46af570ae
phitonthel <thephitonthel@gmail.com>
2021-01-02 20:02:07 +0700

    release 7

--- controllers/controller.js
+++ controllers/controller.js
@@ -18,11 +18,48 @@ class Controller {
   }
 
   static registerCustomer(req, res) {
+    res.render('registerCustomers')
+  }
 
+  static registerCustomerPOST(req, res) {
+    let input = req.body
+    let obj = {}
+    console.log(input);
+    Customer.create(input)
+    .then(() => {
+      res.redirect('/customers')
+    })
+    .catch((err) => {
+      res.send(err)
+    })
   }
 
   static editCustomer(req, res) {
+    let id = +req.params.idCustomer
+    Customer.findByPk(id)
+    .then((data) => {
+      // data.birthDate = data.birthDate.toString()
+      let newDate = data.birthDate.toISOString().slice(0,10);
+      data.dataValues.birthDate = newDate
+      // console.log(data);
+      res.render('editCustomers', {data})
+    })
+    .catch((err) => {
+      res.send(err)
+    })
+  }
 
+  static editCustomerPOST(req, res) {
+    let id = req.params.idCustomer
+    let input = req.body
+    // console.log(input);
+    Customer.update(input, {where:{id:id}})
+    .then((data) => {
+      res.redirect('/customers')
+    })
+    .catch((err) => {
+      res.send(err)
+    })
   }
 
   static showCustomerAccount(req, res) {
--- routes/routes.js
+++ routes/routes.js
@@ -4,8 +4,13 @@ const Controller = require('../controllers/controller')
 
 router.get('/', Controller.home)
 router.get('/customers', Controller.showCustomers)
+
 router.get('/customers/register', Controller.registerCustomer)
+router.post('/customers/register', Controller.registerCustomerPOST)
+
 router.get('/customers/:idCustomer/editProfile', Controller.editCustomer)
+router.post('/customers/:idCustomer/editProfile', Controller.editCustomerPOST)
+
 router.get('/customers/:idCustomer/accounts', Controller.showCustomerAccount)
 router.get('/customers/:idCustomer/accounts/:idAccount/transfer', Controller.transfer)
 
